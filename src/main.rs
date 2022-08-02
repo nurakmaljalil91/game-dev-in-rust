0a6587adaed88872b450f006b5b2348b9b79cbea
Nur Akmal Jalil <nurakmaljalil91@gmail.com>
2022-08-02 16:34:24 +0800

    Add resources

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use bevy::prelude::*;
 
-fn hello_world(){
-    println!("Hello World");
-}
+// fn hello_world(){
+//     println!("Hello World");
+// }
 
 #[derive(Component)]
 struct Person;
@@ -17,9 +17,15 @@ fn add_people(mut commands: Commands){
     commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>){
-    for name in query.iter(){
-        println!("hello {}!", name.0)
+struct GreetTimer(Timer);
+
+fn greet_people(time:Res<Time>, mut timer:ResMut<GreetTimer>, query: Query<&Name, With<Person>>){
+    // update our timer with the time elapsed since last updated
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished(){
+        for name in query.iter(){
+            println!("hello {}!", name.0)
+        }
     }
 }
 
@@ -27,8 +33,8 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people)
-            .add_system(hello_world)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
             .add_system(greet_people);
     }
 }
